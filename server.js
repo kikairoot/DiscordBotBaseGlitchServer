c9431d92b3f7e254bfb5d180bf6d75085b83126b
Glitch (caterwauling-versed-whippoorwill) <none>
2023-11-04 14:47:33 +0000

    📇🚕 Updated with Glitch

--- server.js
+++ server.js
@@ -1,14 +1,75 @@
 const http = require("http");
 const querystring = require("querystring");
 //const discord = require("discord.js");
-const { Client, Intents } = require('discord.js');
+const { 
+  Client,
+  GatewayIntentBits,
+  ApplicationCommandOption,
+  ApplicationCommandOptionType
+} = require('discord.js');
 
 // const client = new discord.Client();
 const client = new Client({
-  intents: [Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGE_TYPING] 
-  ,partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']
+  intents: [
+    GatewayIntentBits.DirectMessages, 
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.GuildMembers, 
+    GatewayIntentBits.GuildMessageTyping
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']
 });
 
+// command
+const nyan = {
+  name:"nyan",
+  description:"にゃ～ん",
+};
+const get_role_list = {
+  name:"get_role_list",
+  description:"get role list",
+  options: [{
+    type: 5, //BOOLEAN
+    name: "view_id",
+    description: "ID表示",
+    required: false,
+  }],
+};
+
+const test_guild_id = "341100652501532675";
+const get_guild_role_list = {
+  name:"get_guild_role_list",
+  description:"get guild role list",
+  options: [{
+    type: ApplicationCommandOptionType.String, //
+    name: "guild_id",
+    description: "ID表示",
+    required: true,
+  },{
+    type: ApplicationCommandOptionType.Boolean, //BOOLEAN
+    name: "view_id",
+    description: "ID表示",
+    required: false,
+  }],
+};
+const get_role_member = {
+  name:"get_role_member",
+  description:"get_role_member",
+  options: [{
+    type: 8, //role
+    name: "role",
+    description: "メンバー一覧を取得するロール",
+    required: true,
+  },{
+    type: ApplicationCommandOptionType.Boolean, //BOOLEAN
+    name: "view_id",
+    description: "ID表示",
+    required: false,
+  }],
+};
+const commands = [nyan, get_role_list, get_role_member];
+const guildCommands =[get_guild_role_list];
+// 外部からのアクセス
 http
   .createServer(function (req, res) {
     if (req.method == "POST") {
@@ -16,33 +77,57 @@ http
       req.on("data", function (chunk) {
         data += chunk;
       });
-      req.on("end", function () {
+      
+      req.on("end", async function () {
         if (!data) {
           res.end("No post data");
           return;
         }
         var dataObject = querystring.parse(data);
         console.log("post:" + dataObject.type);
+        
+        // Glitchのサービスがスリープにならないよう外部から起こす部分
         if (dataObject.type == "wake") {
           console.log("Woke up in post");
           res.end();
           return;
         }
 
+        // チャンネルへのメッセージ送信
         if (dataObject.type == "send") {
           sendMessage(dataObject);
         }
+        // 個人へのダイレクトメッセージ送信
         if (dataObject.type == "sendDM") {
           sendDirectMessage(dataObject);
         }
+        
+        // Discordサーバの情報取得
         if (dataObject.type == "guilds") {
-          getGuilds(dataObject);
+          let result = getGuilds(dataObject);
+          
+          console.log("request guilds");
+          console.log(result);
+          res.end(result);
+          return;
         }
+        // サーバロール一覧取得
         if (dataObject.type == "guildRole" || dataObject.type == "guildRoleByName") {
-          getGuildRole(dataObject);
+          let result = getGuildRole(dataObject);
+          
+          console.log("request guildRole");
+          console.log(result);
+          res.end(result);
+          return;
         }
+        // サーバロールに所属するメンバー一覧取得
         if (dataObject.type == "roleMembers") {
-          getRoleMembers(dataObject);
+          let result = await getRoleMembers(dataObject);
+          
+          console.log("request roleMembers");
+          console.log(result);
+          res.end(result);
+          return;
         }
         
         res.end();
@@ -57,8 +142,70 @@ http
 client.on("ready", (message) => {
   console.log("Bot準備完了～");
   client.user.setPresence({ activity: { name: "げーむ" } });
+
+  client.application.commands.set(commands)
+  //.then(console.log)
+  .catch(console.error);
+  
 });
 
+// slash command resp
+client.on("interactionCreate" ,async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'nyan') {
+    await interaction.reply('にゃ～ん');
+  }
+  if (interaction.commandName === 'get_role_list') {
+    let text = "name\n";
+    if(interaction.guild == null){
+      text = "サーバ専用コマンドです。";
+    }else{
+      const id = interaction.options.get("view_id");
+      console.log(id);
+      if(!id){
+        text = "id," + text;
+      }
+      
+      interaction.guild.roles.cache.forEach(role => {
+        if(!id){
+          text = text + role.id + ",";
+        }
+        text = text + role.name + "\n";
+      });
+    }
+    await interaction.reply(text);
+  }
+  if (interaction.commandName === 'get_role_member') {
+    let text = "nickname,displayName\n";
+    if(interaction.guild == null){
+      text = "サーバ専用コマンドです。";
+    }else{
+      const inputRole = interaction.options.get("role");
+      console.log(inputRole.role.id);
+
+      const id = interaction.options.get("view_id");
+      console.log(id);
+      if(!id){
+        text = "id," + text;
+      }
+      
+
+      await interaction.guild.members.fetch(); // member init (role member init)
+      const role = interaction.guild.roles.cache.get(inputRole.role.id);
+      role.members.forEach((member ,key,map) => {
+          if(!id){
+            text = text + member.id + ",";
+          }
+          text = text + member.nickname + "," + member.displayName + "\n";
+        });
+    }
+    console.log(text);
+    await interaction.reply(text);
+  }
+});
+
+//　Discordのメッセージ送信に反応する
 client.on("messageCreate", (message) => {
   console.log("message channel type text:" + message.channel.type );
   console.log("message content text:" + message.content );
@@ -76,30 +223,6 @@ client.on("messageCreate", (message) => {
     let text = "にゃ～ん";
     console.log(text);
     sendMsg(message.channelId, text);
-    return;
-  }
-  
-  if (message.content.match(/^get_role_member_csv /)){
-    message.guilds.cache.get();
-
-    var guild = client.guilds.cache.get(dataObject.guildId);
-    if(guild != null){
-      console.log("guild(id,name):" + guild.id + "," + guild.name);
-
-      guild.members.fetch(); // member init (role member init)
-      guild.roles.fetch(dataObject.roleId)
-        .then(role => {
-          console.log("role(id.name):" + role.id + "," + role.name);
-          console.log("members:" + role.members);
-          console.log("members size:" + role.members.size);
-
-          role.members.forEach((member ,key,map) => {
-            console.log("role member(id.name,displayName):" + member.id + "," + member.nickname + "," + member.displayName);
-          });
-        })
-        .catch(console.error);
-
-    }
     return;
   }
 });
@@ -168,54 +291,94 @@ function sendDirectMessage(dataObject){
     }
   }
 }
+
+// get server name
 function getGuilds(dataObject){
+  let text = '{"data":';
   console.log("guilds list request");
   client.guilds.cache.forEach(guild => {
     console.log("guild(id,name):" + guild.id + "," + guild.name);
+    text = text + '{"id":"' + guild.id + '","name":"' + guild.name + '"}';
   });
+  text = text + "}";
+  return text;
 }
+
+// get role list by server
 function getGuildRole(dataObject){
   console.log("guild role request");
+  var guild = null;
   if(dataObject.guildId != null){
     console.log("guildId:" + dataObject.guildId);
-
-    var guild = client.guilds.resolve(dataObject.guildId);
-    if(guild != null){
-      console.log("guild(id,name):" + guild.id + "," + guild.name);
-      guild.roles.cache.forEach(role => {
-        console.log("role(id.name):" + role.id + "," + role.name);
-      });
-    }
+    guild = client.guilds.resolve(dataObject.guildId);
+  }else if(dataObject.guildName != null){
+    console.log("guildName:" + dataObject.guildName);
+    guild = client.guilds.cache.find(guild => guild.name == dataObject.guildName);
   }
+  
+  if(guild != null){
+    let data = "";
+    console.log("guild(id,name):" + guild.id + "," + guild.name);
+    guild.roles.cache.forEach(role => {
+      console.log("role(id.name):" + role.id + "," + role.name);
+
+      if(data != ""){
+        data = data + ",";
+      }
+      data = data + '{"id":"' + role.id + '","name":"' + role.name + '"}';
+    });
+    let text = '{"data":[' + data + "]}";
+    return text;
+  }
+  return "{}";
 }
 
+// メンバー一覧取得時に動作の確定が必要なため、asyncが必要
 async function getRoleMembers(dataObject){
   console.log("role members request");
+  var guild = null;
   if(dataObject.guildId != null){
     console.log("guildId:" + dataObject.guildId);
+    guild = client.guilds.cache.get(dataObject.guildId);
+  }else if(dataObject.guildName != null){
+    console.log("guildName:" + dataObject.guildName);
+    guild = client.guilds.cache.find(guild => guild.name == dataObject.guildName);
+  }
+  
+  if(guild != null){
+    console.log("guild(id,name):" + guild.id + "," + guild.name);
+    var role = null;
+
+    // ロールメンバー一覧は、サーバのメンバー一覧取得をしておかないと名前が取得できない（外部呼出しの場合）
+    await guild.members.fetch(); // member init (role member init)
 
     if(dataObject.roleId != null){
       console.log("roleId:" + dataObject.roleId);
-      
+      role = guild.roles.cache.find(role => role.id == dataObject.roleId);
+    }else if(dataObject.roleName){
+      console.log("roleName:" + dataObject.roleName);
+      role = guild.roles.cache.find(role => role.name == dataObject.roleName);
+    }
+    if(role != null){
+      console.log("role(id.name):" + role.id + "," + role.name);
+      console.log("members:" + role.members);
+      console.log("members size:" + role.members.size);
 
-      var guild = client.guilds.cache.get(dataObject.guildId);
-      if(guild != null){
-        console.log("guild(id,name):" + guild.id + "," + guild.name);
-        
-        await guild.members.fetch(); // member init (role member init)
-        guild.roles.fetch(dataObject.roleId)
-          .then(role => {
-            console.log("role(id.name):" + role.id + "," + role.name);
-            console.log("members:" + role.members);
-            console.log("members size:" + role.members.size);
-          
-            role.members.forEach((member ,key,map) => {
-              console.log("role member(id.name,displayName):" + member.id + "," + member.nickname + "," + member.displayName);
-            });
-          })
-          .catch(console.error);
+      let data = "";
+      role.members.forEach((member ,key,map) => {
+        console.log("role member(id.name,displayName):" + member.id + "," + member.nickname + "," + member.displayName);
 
-      }
+        if(data != ""){
+          data = data + ",";
+        }
+        data = data + '{"id":"' + member.id + '","name":"' + member.nickname + '","displayName":"' + member.displayName + '"}';
+      });
+
+      console.log(data);
+      let text = '{"data":[' + data + "]}";
+      console.log(text);
+      return text;
     }
   }
+  return "{}";
 }
